6bb161fd50c7308e86a8dab9ade53afe824b1a23
MerciMathieu <mathieu.delclos@laposte.net>
2018-10-22 20:26:56 +0200

    mathieu

--- public/asserts/js/main.js
+++ public/asserts/js/main.js
@@ -23,7 +23,15 @@ var email = document.getElementById("mail");
         }, false);
       });
     }, false);
-  })();
+
+    $('.btn.btn-primary').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+
+  });
 
  
 
--- view/homeView.php
+++ view/homeView.php
@@ -28,92 +28,91 @@
         </section>
 
         <section class="text-left" id="services">
-        
-                    <h2 class="py-4 m-5 text-center text-uppercase">Services et conseils en informatique</h2>
-                    <div class="blog-card">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/1.png')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Dépannage tout support</h1>
-                            <h2>Windows et Mac</h2>
-                            <p>Nous réparons votre ordinateur quel que soit le support: PC portable, unité centrale, téléphone portable, tablette... <strong>Atelier et domicile</strong> </p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
+            <h2 class="py-4 m-5 text-center text-uppercase">Services et conseils en informatique</h2>
+            <div class="blog-card">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/1.png')"></div>
+                </div>
+                <div class="description">
+                    <h1>Dépannage tout support</h1>
+                    <h2>Windows et Mac</h2>
+                    <p>Nous réparons votre ordinateur quel que soit le support: PC portable, unité centrale, téléphone portable, tablette... <strong>Atelier et domicile</strong> </p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
 
-                    <div class="blog-card alt">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/2.png')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Conseils personnalisés</h1>
-                            <h2>Par mail ou telephone</h2>
-                            <p>Pour un achat, une configuration, périphériques, gaming, musique assistée par ordinateur...etc</p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
+            <div class="blog-card alt">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/2.png')"></div>
+                </div>
+                <div class="description">
+                    <h1>Conseils personnalisés</h1>
+                    <h2>Par mail ou telephone</h2>
+                    <p>Pour un achat, une configuration, périphériques, gaming, musique assistée par ordinateur...etc</p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
 
-                    <div class="blog-card">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/3.jpg')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Montage unité centrale</h1>
-                            <h2>Adapté à vos besoins</h2>
-                            <p>Nous montons et assemblons votre ordinateur adapté à vos besoins: utilisation classique, gaming, M.A.O...</p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
+            <div class="blog-card">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/3.jpg')"></div>
+                </div>
+                <div class="description">
+                    <h1>Montage unité centrale</h1>
+                    <h2>Adapté à vos besoins</h2>
+                    <p>Nous montons et assemblons votre ordinateur adapté à vos besoins: utilisation classique, gaming, M.A.O...</p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
 
-                    <div class="blog-card alt">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/4.png')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Installation et nettoyage du systeme d'exploitation</h1>
-                            <h2>Windows et Mac</h2>
-                            <p></p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
+            <div class="blog-card alt">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/4.png')"></div>
+                </div>
+                <div class="description">
+                    <h1>Installation et nettoyage du systeme d'exploitation</h1>
+                    <h2>Windows et Mac</h2>
+                    <p></p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
 
-                    <div class="blog-card">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/5.jpg')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Amélioration des performances de votre machine</h1>
-                            <h2>Windows et Mac</h2>
-                            <p></p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
+            <div class="blog-card">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/5.jpg')"></div>
+                </div>
+                <div class="description">
+                    <h1>Amélioration des performances de votre machine</h1>
+                    <h2>Windows et Mac</h2>
+                    <p></p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
 
-                    <div class="blog-card alt">
-                        <div class="meta">
-                            <div class="photo" style="background-image: url('asserts/img/6.jpg')"></div>
-                        </div>
-                        <div class="description">
-                            <h1>Récupération et sauvegarde de données </h1>
-                            <h2>(Serveur N.A.S.)</h2>
-                            <p>Votre disque dur a grillé ou peine à fonctionner, nous pouvons essayer de récupérer vos données avant que votre disque dur ne grille!</p>
-                            <p class="read-more">
-                                <a href="#">Read More</a>
-                            </p>
-                        </div>
-                    </div>
-                    <p class="text-center">Et bien plus encore! <br /> N'hésitez pas à nous contacter par téléphone au <strong>06 36 48 38 29</strong> ou via <a href="/contact">le formulaire de contact</a></p>
+            <div class="blog-card alt">
+                <div class="meta">
+                    <div class="photo" style="background-image: url('asserts/img/6.jpg')"></div>
+                </div>
+                <div class="description">
+                    <h1>Récupération et sauvegarde de données </h1>
+                    <h2>(Serveur N.A.S.)</h2>
+                    <p>Votre disque dur a grillé ou peine à fonctionner, nous pouvons essayer de récupérer vos données avant que votre disque dur ne grille!</p>
+                    <p class="read-more">
+                        <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
+            <p class="text-center">Et bien plus encore! <br /> N'hésitez pas à nous contacter par téléphone au <strong>06 36 48 38 29</strong> ou via <a href="/contact">le formulaire de contact</a></p>
         </section> 
     </article> 
 </main>   
