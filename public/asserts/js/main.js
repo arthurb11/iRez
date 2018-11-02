7080bff2d0a6fceead033d7d99d00c19b4f3616d
arthurb11 <arthur.bravin@live.fr>
2018-11-02 16:30:57 +0100

    smooth scroll ok

--- public/asserts/js/main.js
+++ public/asserts/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
-  
+    console.log('tg');
+
+    $(".btn-primary").click(function() {
+        $('html').animate({
+            scrollTop: $("#services").offset().top},
+            'slow');
+    });
   
 });
