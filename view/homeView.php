920e64f15ff630176baec39ba2e4caaf87def9aa
arthurb11 <arthur.bravin@live.fr>
2018-11-09 02:14:14 +0100

    responsive final

--- view/homeView.php
+++ view/homeView.php
@@ -19,14 +19,14 @@
 <!--                                <h1 class="text-uppercase">Solutions Informatiques</h1>-->
                                 <img src="../asserts/img/logopicto.png" class="img-fluid">
                                 <p class="text-md-center">Spécialisé depuis plus de 10 ans dans le domaine informatique, nous vous proposons des services en <strong>atelier ainsi qu'à domicile</strong>. 
-                                    Nous sommes situés à Aubagne(13) dans la zone des Paluds.<br />
-                                    Dépannage de votre machine, assemblage de votre nouveau PC, sauvegarde de vos données, installation réseau... <strong><br /><br />Nous répondrons à tous vos besoins.</strong><br /><br />
-                                    <a href="#services" class="btn btn-primary m-3 js-scrollTo">Continuer</a>
-                                    <!--
-<a id="buttonlogo" href="#services" class="js-scrollTo">
-<img class="img-fluid" src="../asserts/img/logobutton.png">
-</a>
--->
+                                Nous sommes situés à Aubagne(13) dans la zone des Paluds.<br />
+                                Dépannage de votre machine, assemblage de votre nouveau PC, sauvegarde de vos données, installation réseau... <strong><br /><br />Nous répondrons à tous vos besoins.</strong><br /><br />
+                                <a href="#services" class="btn btn-primary m-3 js-scrollTo">Continuer</a>
+                                <!--
+                                <a id="buttonlogo" href="#services" class="js-scrollTo">
+                                <img class="img-fluid" src="../asserts/img/logobutton.png">
+                                </a>
+                                -->
                             </div>
                         </div>
                     </div>
